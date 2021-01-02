af11c1b3fd37ecec46d7a6457dc1af0006a6d62d
GaMerZ1911 <76888598+GaMerZ1911@users.noreply.github.com>
2021-01-03 01:59:10 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Jan 3rd, 2006',
+  name: 'IOANA'
 };
 
 function hideEverything() {
